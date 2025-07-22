fca9a96e6fbd12cf3450088201b6b470012f0ccc
Yura Subotinov <crapmindd@gmail.com>
2025-07-22 13:25:50 +0300

    feat: add returnRentalFilm method to CustomerService

--- src/main/java/dev/subotinov/service/imlp/CustomerServiceImpl.java
+++ src/main/java/dev/subotinov/service/imlp/CustomerServiceImpl.java
@@ -2,10 +2,14 @@ package dev.subotinov.service.imlp;
 
 import dev.subotinov.config.JpaUtil;
 import dev.subotinov.entity.Customer;
+import dev.subotinov.entity.store.Rental;
 import dev.subotinov.service.CustomerService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 import lombok.NonNull;
+
+import java.time.LocalDate;
 
 public class CustomerServiceImpl implements CustomerService {
 
@@ -24,6 +28,30 @@ public class CustomerServiceImpl implements CustomerService {
                 if (transaction.isActive()) transaction.rollback();
                 System.out.println(e.getMessage());
                 throw e;
+            }
+        }
+    }
+    @Override
+    public void returnRentalFilm(@NonNull Customer customer) {
+        try (EntityManager em = JpaUtil.getEntityManager()) {
+            try {
+                em.getTransaction().begin();
+
+                TypedQuery<Rental> query = em.createQuery(
+                        "SELECT r FROM Rental r WHERE r.customer = :customer AND r.returnDate IS NULL",
+                        Rental.class
+                );
+                query.setParameter("customer", customer);
+                Rental rental = query.getResultStream().findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException("Нет незакрытой аренды у покупателя"));
+
+                rental.setReturnDate(LocalDate.now());
+                em.merge(rental);
+
+                em.getTransaction().commit();
+            }catch (Exception e) {
+                em.getTransaction().rollback();
+                System.out.println(e.getMessage());
             }
         }
     }
